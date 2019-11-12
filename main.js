745afa4d4967ba588fed0b483659495a555ea7b1
michele <michele.provolo@gmail.com>
2019-11-12 13:01:29 +0100

    inserito input verde

--- main.js
+++ main.js
@@ -1,9 +1,29 @@
+$( document ).ready(function() {
+
+    //agganciamo al click sul bottone la funzione di callbnack
+    $(".sendmsg").click(function(){
+
+    //ci salviamo il valore dell'input inserito dall'utente
+    var messaggio = $(".message").val();
+    // console.log(messaggio);
+    
+    // cloniamo (facciamo una copia) del div con classe "msgsent" che sta dentro
+    // un div con ID "template"
+    var elmentmsg = $("#template .msgsent").clone();
+
+    console.log(elmentmsg);
+
+    // modifica questa copia di "msgsent" aggiungendogli il testo del messaggio
+    elmentmsg.find(".testo").text(messaggio);
+    
+    // appendiamo una copia con testo valorizzato del div "msgsent"
+    $(".green-msg").append(elmentmsg);
+
+    // ripuliamo il contenuto dell'input, per UX
+    $(".message").val("");
 
 
-
-
-$(document).ready(function () {
-
+    });
 
 
 });
